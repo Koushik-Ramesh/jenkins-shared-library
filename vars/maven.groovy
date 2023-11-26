f8c42dd503fed36e02fd8f1f8c03707d5453c52c
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-11-27 00:25:47 +0530

    Autopush

---
+++ vars/maven.groovy
@@ -0,0 +1,27 @@
+def lintchecks() {
+    sh "echo Starting lintchecks for ${Component}"
+    sh "mvn checkstyle:check || true"
+    sh "echo lintchecks completed for ${Component}"
+}
+
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('lint checks') {
+                steps {
+                    script {
+                        lintchecks()
+                    }
+                    
+                }
+            }
+            stage("Generating Artifacts") {
+                steps {
+                    sh "echo Generating Artifacts"
+                    sh "npm install"
+                }
+            }
+        }
+    }
+}
