30d1a83ec87adaaeb2cbd92e51dcd07a29e72a6a
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-11-26 21:08:22 +0530

    Autopush

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,14 +1,21 @@
+def lintchecks() {
+    sh "echo Installing JSlist"
+    sh "npm i jslint"
+    sh "echo Starting lintchecks..."
+    sh "node_modules/jslint/bin/jslint.js server.js || true"
+    sh "echo lintchecks completed...."
+}
+
 def call() {
     pipeline {
         agent any
         stages {
             stage('lint checks') {
                 steps {
-                    sh "echo Installing JSlist"
-                    sh "npm i jslint"
-                    sh "echo Starting lintchecks..."
-                    sh "node_modules/jslint/bin/jslint.js server.js || true"
-                    sh "echo lintchecks completed...."
+                    script {
+                        lintchecks()
+                    }
+                    
                 }
             }
             stage("Generating Artifacts") {
