965d35e745c0ca0db83dbf50ab738ccb532b00ab
Koushik-Ramesh <koushik.r9380@gmail.com>
2023-11-26 21:38:23 +0530

    Autopush

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -3,7 +3,7 @@ def lintchecks() {
     sh "npm i jslint"
     sh "echo Starting lintchecks for ${Component}"
     sh "node_modules/jslint/bin/jslint.js server.js || true"
-    sh "echo lintchecks completed...."
+    sh "echo lintchecks completed for ${Component}"
 }
 
 def call() {
