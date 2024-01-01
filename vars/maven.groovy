5ff116c4f094730fc1a367184c2636bc5874f28e
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-01-01 20:03:15 +0530

    Autopush

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -30,7 +30,7 @@ def call() {
             stage ('Sonar Checks') {
                 steps {
                     sh "echo starting Sonar Checks for ${Component}"
-                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${Component} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}"
+                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.java.binaries=target/ -Dsonar.projectKey=${Component} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}"
                     sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
                     sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${SONAR_URL} ${Component}"
                     sh "echo Sonar Checks for ${Component} completed"
